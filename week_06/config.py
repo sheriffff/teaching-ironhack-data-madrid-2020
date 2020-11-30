58a5b7088200a723716afefcccb01b93cba28510
sheriffff <manuel.lopez.sheriff@gmail.com>
2020-11-30 17:15:30 +0100

    selenium

---
+++ week_06/config.py
@@ -0,0 +1,6 @@
+user_data = {
+    "name": "PEDRO RAMIREZ",
+    "NIF": "12345678Z",
+    "telf": "666555888",
+    "mail": "pedrito@gmail.com"
+}
--- week_06/selenium_utils.py
+++ week_06/selenium_utils.py
@@ -5,27 +5,6 @@ from time import sleep
 log_errors_file_path = "./errors.txt"
 
 
-def accede_formulario(driver, url):
-    driver.get(url)
-    sleep(1)
-    driver.find_element_by_css_selector("div div div input").click()
-    sleep(1)
-    
-def rellena_campos(driver):
-    driver.find_element_by_name("nombreApellidos").send_keys("PEDRO RAMÍREZ")
-
-    driver.find_element_by_name("tipo").send_keys("N")
-
-    driver.find_element_by_name("numeroDocumento").send_keys("12345678A")
-
-    driver.find_element_by_name("telefono").send_keys("602345678")
-
-    driver.find_element_by_name("eMail").send_keys("pedrito69tkm@gmail.com")
-
-    driver.find_element_by_id("radioProvincia").click()
-
-    driver.find_element_by_name("provincia").send_keys("m")
-
 def resuelve_captcha(driver, model):
     _words = driver.find_element_by_css_selector("p.p0").text.split(": ")
     words = [w for w in _words if w != ""]
@@ -61,16 +40,6 @@ def _resuelve_captcha(model, word, words):
         
     return sol
 
-def rellena_captcha_y_siguiente(driver, solution):
-    # print(f'rellenando... {solution}')
-    driver.find_element_by_name("ARQ.CAPTCHA").send_keys(solution)
-
-    driver.find_element_by_name("SPM.ACC.SIGUIENTE").click()
-
-def pensiones_click_and_next(driver):
-    driver.find_element_by_id("335").click()
-
-    driver.find_element_by_name("SPM.ACC.CONTINUAR_TRAS_SELECCIONAR_SERVICIO").click()
 
 def get_most_similar_spanish(word, word_list, model):
     t = Translator()
@@ -119,6 +88,47 @@ def is_number_expression(word):
         return False
     else:
         return number
+
+    
+    
+    
+    
+    
+
+
+
+
+def accede_formulario(driver, url):
+    driver.get(url)
+    sleep(1)
+    driver.find_element_by_css_selector("div div div input").click()
+    sleep(1)
+    
+def rellena_campos(driver):
+    driver.find_element_by_name("nombreApellidos").send_keys("PEDRO RAMÍREZ")
+
+    driver.find_element_by_name("tipo").send_keys("N")
+
+    driver.find_element_by_name("numeroDocumento").send_keys("12345678A")
+
+    driver.find_element_by_name("telefono").send_keys("602345678")
+
+    driver.find_element_by_name("eMail").send_keys("pedrito69tkm@gmail.com")
+
+    driver.find_element_by_id("radioProvincia").click()
+
+    driver.find_element_by_name("provincia").send_keys("m")
+
+def rellena_captcha_y_siguiente(driver, solution):
+    # print(f'rellenando... {solution}')
+    driver.find_element_by_name("ARQ.CAPTCHA").send_keys(solution)
+
+    driver.find_element_by_name("SPM.ACC.SIGUIENTE").click()
+
+def pensiones_click_and_next(driver):
+    driver.find_element_by_id("335").click()
+
+    driver.find_element_by_name("SPM.ACC.CONTINUAR_TRAS_SELECCIONAR_SERVICIO").click()
     
 def is_paso_2(driver):
     try:
@@ -127,3 +137,49 @@ def is_paso_2(driver):
         return False
     else:
         return titulo.text == "Paso 2 de 4. Selección de Servicio."
+    
+    
+    
+    
+def proceso_total():
+
+    accede_formulario(driver, url_ss)
+    
+    rellena_campos(driver)
+
+    solution = resuelve_captcha(driver, model)
+    
+    rellena_captcha_y_siguiente(driver, solution)
+    sleep(.5)
+    
+    pensiones_click_and_next(driver)
+    sleep(.5)
+    
+    if not is_paso_2(driver):
+        return True
+
+while True:
+    sleep(5)
+    try:
+        if proceso_total():
+            print("VAMOS!")
+            effect.play(loops=30)
+            
+            break
+        else:
+            print("NO HAY CITA")
+    except:
+        print("ERROR")
+        pass
+    
+    print("\n"
+          
+          
+          #### Load word vectors
+
+I need a mapping word --> vector
+
+import gensim
+
+%%time
+model = gensim.models.KeyedVectors.load_word2vec_format('../../../Documents/coding/selenium/word2vec.300d.W.pos.vectors', binary=False)
